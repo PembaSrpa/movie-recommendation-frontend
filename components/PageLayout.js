de8d81562a2c071fec9774975c7682fa052fd3ff
PembaSrpa <nirashanichang@gmail.com>
2026-01-04 11:08:27 +0545

    Improved UI with centered layout and side panels 3

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,7 +1,7 @@
 export default function PageLayout({ children }) {
   return (
     <div className="min-h-screen bg-linear-to-br from-slate-900 via-purple-900 to-slate-900 relative overflow-hidden">
-        <div className="w-full lg:w-3/5 px-4 py-8">
+        <div className="w-3/5 mx-auto lg:w-3/5 px-4 py-8">
           {children}
         </div>
     </div>
